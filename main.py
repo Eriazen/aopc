6ec7962af46deb67c9f0168745ebade4e0175875
vecerniv <vita.vecernik@seznam.cz>
2025-08-21 18:37:38 +0200

    restore main.py from pre-PR

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from aopc.ui import market, crafting, transport, item_list, settings
 
 
 def main():
-    pass
+    view = MarketView('https://europe.albion-online-data.com', 'T4_BAG')
+    view.set_data()
+    print(view.get_data())
 
 
 if __name__ == "__main__":
