73956d6ff1c2fe438fc5808418300cf07c2abc2a
vecerniv <vita.vecernik@seznam.cz>
2025-08-21 18:33:02 +0200

    update main.py script

---
+++ main.py
@@ -0,0 +1,11 @@
+from nicegui import ui
+from aopc.io import JSONGetter
+from aopc.ui import market, crafting, transport, item_list, settings
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
